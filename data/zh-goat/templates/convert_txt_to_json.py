c34cdb2be57e436b9b76f71b2904823045e3a7df
yanqiang <yanqiang@ict.ac.cn>
2023-06-02 17:17:38 +0800

    update

---
+++ data/zh-goat/templates/convert_txt_to_json.py
@@ -0,0 +1,13 @@
+import json
+
+# Open the instruction template text file
+with open('template.txt', 'r',encoding='utf-8') as f_in:
+
+    lines = {}
+
+    for i, line in enumerate(f_in):
+        lines[str(i+1)] = line.strip()
+
+# Write the list to a JSON file
+with open('goat.json', 'w',encoding='utf-8') as f_out:
+    json.dump(lines, f_out, indent=4,ensure_ascii=False)
