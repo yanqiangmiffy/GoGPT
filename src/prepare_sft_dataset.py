133d8f5191c8c214e8b94dbb82b12f1144cd5b93
yanqiang <yanqiang@ict.ac.cn>
2023-08-02 18:06:42 +0800

    update

---
+++ src/prepare_sft_dataset.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@author:quincy qiang
+@license: Apache Licence
+@file: prepare_sft_dataset.py
+@time: 2023/08/02
+@contact: yanqiangmiffy@gamil.com
+@software: PyCharm
+@description: coding..
+"""
+
+import datasets
+
+
+dataset=datasets.load_dataset('/data/searchgpt/data/instruction_data/instruction_merge_set/data')
